2244f03b3200ac5c5f06a321ce5d722034a8b721
bonzoman <bonzoman@gmail.com>
2024-12-04 21:48:21 +0900

    asdfasdf

--- collection/array/MyArrayListV1Main.java
+++ collection/array/MyArrayListV1Main.java
@@ -4,7 +4,7 @@ public class MyArrayListV1Main {
 //
     public static void main(String[] args) {
         MyArrayListV1 list = new MyArrayListV1();
-        System.out.println("==데이터 추가==");
+        System.out.println("==데이터 추가=="); //
         System.out.println(list);
         list.add("a");
         System.out.println(list);
